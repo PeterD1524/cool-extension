c540a58d4c53bde23bba5f434bbbb2f635db23a4
PeterD1524 <qaz246135@gmail.com>
2020-10-01 12:55:18 +0800

    Options

--- extension/contentScript.js
+++ extension/contentScript.js
@@ -1,4 +1,5 @@
 let vjs_video_3_html5_api = document.querySelector("#vjs_video_3_html5_api");
+let key = 'coolOptions';
 if (vjs_video_3_html5_api === null) {
     let timeout = 1000;
     console.log(timeout);
@@ -26,23 +27,51 @@ function oyrgy06xEwMPnQhwz_pHiZlItMV05DZcljWs2JMschc() {
     button.title = 'Download';
     button.setAttribute('aria-disabled', false);
     button.addEventListener('click', function () {
-        chrome.runtime.sendMessage(
-            {
-                'chrome.downloads.download': [
-                    {
-                        'url': vjs_video_3_html5_api.src,
-                        'filename': document.querySelector("body > div:nth-child(3) > div > div > h3").textContent.replace(/[\/:]/g, '_'),
+        chrome.storage.sync.get(key, function (items) {
+            let options = {
+                'url': vjs_video_3_html5_api.src,
+                'filename': `${document.querySelector("body > div:nth-child(3) > div > div > h3").textContent}-${document.querySelector("#vjs_video_3 > div.vjs-control-bar > div:nth-child(8) > div > ul > li.vjs-menu-item.vjs-selected > span.vjs-menu-item-text").textContent}`.replace(/[\/\\\:\|\<\>\*\~\?\"]/g, '_').trim(),
+            };
+            if (key in items) {
+                let obj = items[key];
+                for (let name in obj) {
+                    if (name === 'filename') {
+                        obj[name] = obj[name].replace(/\$\{t\}/g, document.querySelector("body > div:nth-child(3) > div > div > h3").textContent);
+                        obj[name] = obj[name].replace(/\$\{q\}/g, document.querySelector("#vjs_video_3 > div.vjs-control-bar > div:nth-child(8) > div > ul > li.vjs-menu-item.vjs-selected > span.vjs-menu-item-text").textContent);
+                        obj[name] = obj[name].replace(/[\/\\\:\|\<\>\*\~\?\"]/g, '_').trim();
+                        options[name] = obj[name];
+                        if (options[name] === '') {
+                            delete options[name];
+                        }
+                    } else if (name === 'conflictAction' || name === 'saveAs') {
+                        if (obj[name] === null || obj[name] === 'unset') {
+                        } else {
+                            options[name] = obj[name];
+                        }
                     }
-                ]
-            },
-            function (response) {
-                if (response === undefined) {
-                    console.log('chrome.runtime.lastError.message', chrome.runtime.lastError.message);
-                } else {
-                    console.log('response', response);
                 }
+            } else {
             }
-        );
+            chrome.runtime.sendMessage(
+                {
+                    'chrome.downloads.download': [
+                        options
+                    ]
+                },
+                function (response) {
+                    if (response === undefined) {
+                        console.log('chrome.runtime.lastError.message', chrome.runtime.lastError.message);
+                    } else {
+                        console.log('response', response);
+                        if ('chrome.runtime.lastError.message' in response) {
+                            if (response['chrome.runtime.lastError.message'] === 'Invalid filename') {
+                                alert(`${response['chrome.runtime.lastError.message']}: ${JSON.stringify(options['filename'])}`);
+                            }
+                        }
+                    }
+                }
+            );
+        });
     })
     let span;
     span = document.createElement('span');
--- extension/options.js
+++ extension/options.js
@@ -2,22 +2,27 @@ let options_div = document.getElementById('options');
 let options = [
     {
         'name': 'filename',
-        'description': "<p>A file path relative to the Downloads directory to contain the downloaded file, possibly containing subdirectories. Absolute paths, empty paths, and paths containing back-references \"..\" will cause an error.</p>"
+        'description': "<p>A file path relative to the Downloads directory to contain the downloaded file, possibly containing subdirectories. Absolute paths, empty paths, and paths containing back-references \"..\" will cause an error.<br>Any character among \"<code>\"*/:<>?\|</code>\" will be replaced with a \"<code>_</code>\".<br><a href=\"https://docs.microsoft.com/en-us/windows/win32/fileio/naming-a-file\">Naming Files, Paths, and Namespaces - Win32 apps | Microsoft Docs</a></p><h3>variables</h3><dd><dl><dt>${t}</dt><dd>Title, i.e. \"<code>document.querySelector(\"body > div:nth-child(3) > div > div > h3\").textContent.</code>\"</dd><dt>${q}</dt><dd>Quality, i.e. \"<code>document.querySelector(\"#vjs_video_3 > div.vjs-control-bar > div:nth-child(8) > div > ul > li.vjs-menu-item.vjs-selected > span.vjs-menu-item-text\").textContent</code>\".</dd></dl></dd>",
+        'default': '${t}-${q}',
     },
     {
         'name': 'conflictAction',
         'description': "<p>The action to take if <code>filename</code> already exists.</p><dd><dl><dt>uniquify</dt><dd>To avoid duplication, the <code>filename</code> is changed to include a counter before the filename extension.</dd><dt>overwrite</dt><dd>The existing file will be overwritten with the new file.</dd><dt>prompt</dt><dd>The user will be prompted with a file chooser dialog.</dd></dl></dd>",
-        'input': { 'name': undefined, 'value': ["uniquify", "overwrite", "prompt"] }
+        'input': { 'name': undefined, 'value': ["uniquify", "overwrite", "prompt"] },
+        'default': 'unset',
     },
     {
         'name': 'saveAs',
         'description': "<p>Use a file-chooser to allow the user to select a filename regardless of whether <code>filename</code> is set or already exists.</p>",
-        'input': { 'name': undefined, 'value': [true, false] }
+        'input': { 'name': undefined, 'value': [true, false] },
+        'default': 'unset',
     }
 ]
+let key = 'coolOptions';
 document.addEventListener('DOMContentLoaded', function () {
     for (let property of options) {
         let div = document.createElement('div');
+        div.id = `options-${property['name']}`;
         div.style.fontSize = '14px';
         div.style.color = '#333';
         div.style.lineHeight = '1.5em';
@@ -42,6 +47,9 @@ document.addEventListener('DOMContentLoaded', function () {
         for (let dl of description.querySelectorAll('dl')) {
             dl.style.margin = '.8em 0';
         }
+        for (let p of description.querySelectorAll('p')) {
+            p.style.marginBottom = '1em';
+        }
         description.style.marginBottom = '1em';
         description.style.color = '#555';
         div.appendChild(description);
@@ -49,6 +57,7 @@ document.addEventListener('DOMContentLoaded', function () {
             let textarea = document.createElement('textarea');
             textarea.style.width = '100%';
             textarea.style.resize = 'none';
+            textarea.spellcheck = false;
             let observer = new MutationObserver(function (mutationsList, observer) {
                 mutationsList.forEach(function (mutation) {
                     for (let node of mutation.addedNodes.values()) {
@@ -127,6 +136,7 @@ document.addEventListener('DOMContentLoaded', function () {
     span.textContent = 'Save';
     span.style.verticalAlign = 'middle';
     button.appendChild(span);
+    button.addEventListener('click', save_options);
     div.appendChild(button);
     button = document.createElement('button');
     button.className = 'btn btn-danger';
@@ -140,6 +150,74 @@ document.addEventListener('DOMContentLoaded', function () {
     span.textContent = 'Reset';
     span.style.verticalAlign = 'middle';
     button.appendChild(span);
+    button.addEventListener('click', function () {
+        set_options(default_option_obj());
+    });
     div.appendChild(button);
     options_div.appendChild(div);
+    restore_options();
 });
+function restore_options() {
+    chrome.storage.sync.get(key, function (items) {
+        if (key in items) {
+            set_options(items[key]);
+        } else {
+            set_options(default_option_obj());
+        }
+    });
+}
+function default_option_obj() {
+    let obj = {};
+    options.forEach(function (value) {
+        obj[value['name']] = value['default'];
+    });
+    return obj;
+}
+function set_options(obj) {
+    for (let property of options) {
+        let div = document.getElementById(`options-${property['name']}`);
+        if (property['name'] === 'filename') {
+            div.querySelector('textarea').value = obj[property['name']];
+        } else if (property['name'] === 'conflictAction' || property['name'] === 'saveAs') {
+            if (obj[property['name']] === null) {
+                let inputChecked = div.querySelector('input:checked');
+                if (inputChecked !== null) {
+                    inputChecked.checked = false;
+                }
+            } else {
+                div.querySelector(`input[value=${obj[property['name']]}]`).checked = true;
+            }
+        }
+    }
+}
+function save_options() {
+    let items = {};
+    items[key] = {};
+    for (let property of options) {
+        let div = document.getElementById(`options-${property['name']}`);
+        let value;
+        if (property['name'] === 'filename') {
+            value = div.querySelector('textarea').value;
+        } else if (property['name'] === 'conflictAction' || property['name'] === 'saveAs') {
+            let inputChecked = div.querySelector('input:checked');
+            if (inputChecked === null) {
+                value = null;
+            } else {
+                value = inputChecked.value;
+            }
+            if (property['name'] === 'saveAs') {
+                if (value === 'true' || value === 'false') {
+                    value = JSON.parse(value);
+                }
+            }
+        }
+        items[key][property['name']] = value;
+    }
+    chrome.storage.sync.set(items, function () {
+        if ('lastError' in chrome.runtime) {
+            console.log('chrome.runtime.lastError.message', chrome.runtime.lastError.message);
+        } else {
+
+        }
+    });
+}
